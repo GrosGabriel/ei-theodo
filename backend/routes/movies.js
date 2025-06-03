ce16a1da4662723ba4ff868606bc6fe4de5591b5
Gabriel Gros <gabriel.gros@student-cs.fr>
2025-06-03 15:10:47 +0200

    delete movie

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -88,24 +88,7 @@ router.post('/new', function (req, res) {
         // id: savedMovie.identifiers[0].id, // Assuming the ID is returned in the identifiers array
       });
     })
-  // movieRepository
-  //   .save(newMovie)
-  //   .then(function (savedMovie) {
-  //     res.status(201).json({
-  //       message: 'Movie successfully created',
-  //       id: savedMovie.id,
-  //     });
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //     if (error.code === '23505') {
-  //       res.status(400).json({
-  //         message: `Movie with title "${newMovie.title}" already exists`,
-  //       });
-  //     } else {
-  //       res.status(500).json({ message: 'Error while creating the movie' });
-  //     }
-  //   });
+
 });
 
 router.delete('/:movieId', function (req, res) {
@@ -113,7 +96,7 @@ router.delete('/:movieId', function (req, res) {
     .getRepository(Movie)
     .delete({ id: req.params.movieId })
     .then(function () {
-      res.status(204).json({ message: 'Movie successfully deleted' });
+      res.status(200).json({ message: 'Movie successfully deleted' });
     })
     .catch(function () {
       res.status(500).json({ message: 'Error while deleting the movie' });
