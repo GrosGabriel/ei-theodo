e941cbca1c68c5cda66cfa85a332a9873c1c12b6
Gabriel Gros <gabriel.gros@student-cs.fr>
2025-06-04 15:29:35 +0200

    LES NOTES

--- frontend/src/pages/MovieDetails/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails/MovieDetails.jsx
@@ -15,7 +15,9 @@ function MovieDetails() {
     }
     return null;
   }
-  const [movieRating, setMovieRating] = useState(0);
+  const [movieRating, setMovieRating] = useState("");
+  useEffect(() => {
+    fetchUserNote();},[]);
   
   async function fetchUserNote() {
     const email = localStorage.getItem('savedEmail');
@@ -23,9 +25,9 @@ function MovieDetails() {
     const userId = await getUserIdByEmail(email);
     if (!userId) return;
     try {
-      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/users/${userId}/note?movie=${id}`);
-      if (res.data && res.data.note !== undefined) {
-        setMovieRating(res.data.note);
+      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/notes/search?userid=${userId}&filmid=${id}`);
+      if (res.data && res.data.notes[0].note !== undefined) {
+        setMovieRating(res.data.notes[0].note);
       }
     } catch (e) {
       setMovieRating(0); // ou null si tu préfères
