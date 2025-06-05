ff01727fa6a18265d1732fe40406deb9b6ac03b5
Gabriel Gros <gabriel.gros@student-cs.fr>
2025-06-05 11:55:08 +0200

    un commit fin de matin

--- backend/similarite.py
+++ backend/similarite.py
@@ -237,7 +237,7 @@ def get_recommandations(user_id, N=5):
     return resultat
 
 
-def recommandations_json(user_id,N=5):
+def recommandations_json(user_id,N=3):
     recos = get_recommandations(user_id, N)
     res = []
     dico_movie = movie_info_dict()
