d90d6cfbbd994a6d1e6f65df88933e89a4aa5a44
Gabriel Gros <gabriel.gros@student-cs.fr>
2025-06-05 19:58:44 +0200

    Suppression de counter

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -9,10 +9,7 @@ const Header = () => {
       <Link className="Link" to="/">
         Home
       </Link>
-      <div></div>
-      <Link className="Link" to="/counter">
-        Counter
-      </Link>
+
       <div></div>
       <Link className="Link" to="/users">
         Users
